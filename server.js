eb277d331d4df2c97af8063e88850955a00326c3
ngthing <ngthi.nguyen21@gmail.com>
2016-10-27 11:20:08 -0400

    HW7

--- server.js
+++ server.js
@@ -1,25 +1,36 @@
 /**
  * Created by thinguyen on 10/27/16.
  */
-var express = require('express');
-var gcloud = require('google-cloud');
-var firebase = require('firebase');
-var multer = require("multer");
-var uploader = multer({ storage: multer.memoryStorage({}) });
-var app = express();
-var port = process.env.PORT || 4000;
+var http = require('http');
+var server = http.createServer(function(req,res){
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end('<h1>Hello World</h1>');
 
-var counter = 0;
-app.get('/', function (req, res) {
-    res.send('Hello World has been said ' + counter + ' times!');
-    counter++;
 });
-
-app.listen(port, function () {
-    console.log('Example app listening on port' + port);
-});
-
-app.use(express.static('public')); // App load static files e.i. html, css, js
-app.listen(port, function () {
-    console.log('Example app listening on port ' + port + '!');
-});
+var port = Number(process.env.PORT || 3000);
+server.listen(port);
+// var express = require('express');
+// var gcloud = require('google-cloud');
+// var firebase = require('firebase');
+// var multer = require("multer");
+// var uploader = multer({ storage: multer.memoryStorage({}) });
+// var app = express();
+// var port = process.env.PORT || 3000;
+// var bodyParser = require('body-parser')
+//
+// app.use(bodyParser.json());       // to support JSON-encoded bodies
+// app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+//     extended: true
+// }));
+//
+//
+// var counter = 0;
+// app.get('/', function (req, res) {
+//     res.send('Hello World has been said ' + counter + ' times!');
+//     counter++;
+// });
+//
+// app.use(express.static('public')); // App load static files e.i. html, css, js
+// app.listen(port, function () {
+//     console.log('Example app listening on port ' + port + '!');
+// });
