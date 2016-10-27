fcc09def09f5a765e56262da45e30e763c0b706a
ngthing <ngthi.nguyen21@gmail.com>
2016-10-27 09:12:26 -0400

    HW7

--- server.js
+++ server.js
@@ -7,6 +7,19 @@ var firebase = require('firebase');
 var multer = require("multer");
 var uploader = multer({ storage: multer.memoryStorage({}) });
 var app = express();
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 4000;
+
+var counter = 0;
+app.get('/', function (req, res) {
+    res.send('Hello World has been said ' + counter + ' times!');
+    counter++;
+});
+
+app.listen(port, function () {
+    console.log('Example app listening on port' + port);
+});
 
 app.use(express.static('public')); // App load static files e.i. html, css, js
+app.listen(port, function () {
+    console.log('Example app listening on port ' + port + '!');
+});
