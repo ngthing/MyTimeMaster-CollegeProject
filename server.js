774923a9d29b6b18d133186163e0f2f81e0c8e14
ngthing <ngthi.nguyen21@gmail.com>
2016-10-27 21:25:16 -0400

    HW7

--- server.js
+++ server.js
@@ -3,12 +3,6 @@
  */
 var http = require('http');
 var express = require('express');
-
-// var app = http.createServer(function(req,res){
-//     res.writeHead(200, {'Content-Type': 'text/html'});
-//     res.end('<h1>Hello from MyTimeMaster</h1>');
-//
-// });
 var app = express();
 var port = Number(process.env.PORT || 3000);
 var counter = 0;
@@ -19,29 +13,6 @@ app.get('/', function (req, res) {
 
 app.listen(port);
 
-//app.use(express.static('public')); // App load static files e.i. html, css, js
+app.use(express.static('public')); // App load static files e.i. html, css, js
 
 
-// var gcloud = require('google-cloud');
-// var firebase = require('firebase');
-// var multer = require("multer");
-// var uploader = multer({ storage: multer.memoryStorage({}) });
-// var app = express();
-// var port = process.env.PORT || 3000;
-// var bodyParser = require('body-parser')
-//
-// app.use(bodyParser.json());       // to support JSON-encoded bodies
-// app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-//     extended: true
-// }));
-//
-//
-// var counter = 0;
-// app.get('/', function (req, res) {
-//     res.send('Hello World has been said ' + counter + ' times!');
-//     counter++;
-// });
-//
-// app.listen(port, function () {
-//     console.log('Example app listening on port ' + port + '!');
-// });
