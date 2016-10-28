681265ef91317ae537110546d0dce7e08c4b40de
ngthing <ngthi.nguyen21@gmail.com>
2016-10-28 11:56:05 -0400

    HW7

--- server.js
+++ server.js
@@ -8,18 +8,60 @@ var gcloud = require('google-cloud');
 
 var app = express();
 var port = Number(process.env.PORT || 3000);
-var counter = 0;
+
 
 firebase.initializeApp({
     serviceAccount: "MyTimeMaster-07379faeb028.json",
     databaseURL: "https://mytimemaster.firebaseio.com"
 });
 
-
-app.get('/', function (req, res) {
-    res.send('Hello World has been said ' + counter + ' times!');
-    counter++;
+/**
+ * Google cloud storage part
+ */
+var CLOUD_BUCKET="mytimemaster.appspot.com"; //From storage console, list of buckets
+var gcs = gcloud.storage({
+    projectId: '17686327988', //from storage console, then click settings, then "x-goog-project-id"
+    keyFilename: 'MyTimeMaster-07379faeb028.json' //the key we already set up
 });
+
+function getPublicUrl (filename) {
+    return 'https://storage.googleapis.com/' + CLOUD_BUCKET + '/' + filename;
+}
+var bucket = gcs.bucket(CLOUD_BUCKET);
+
+//From https://cloud.google.com/nodejs/getting-started/using-cloud-storage
+function sendUploadToGCS (req, res, next) {
+    if (!req.file) {
+        return next();
+    }
+
+    var gcsname = Date.now() + req.file.originalname;
+    var file = bucket.file(gcsname);
+
+
+    var stream = file.createWriteStream({
+        metadata: {
+            contentType: req.file.mimetype
+        }
+    });
+
+    stream.on('error', function (err) {
+        req.file.cloudStorageError = err;
+        next(err);
+    });
+
+    stream.on('finish', function () {
+        req.file.cloudStorageObject = gcsname;
+        req.file.cloudStoragePublicUrl = getPublicUrl(gcsname);
+        var options = {
+            entity: 'allUsers',
+            role: gcs.acl.READER_ROLE
+        };
+        file.acl.add(options, function(a,e){next();});//Make file world-readable; this is async so need to wait to return OK until its done
+    });
+
+    stream.end(req.file.buffer);
+}
 
 app.listen(port);
 
